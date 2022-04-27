8eeac3e100c85b686cef9cc1fc5da87e3a625031
JimComia <103912229+JimComia@users.noreply.github.com>
2022-04-27 16:33:30 +0800

    for sound effects

--- SkySweeper.py
+++ SkySweeper.py
@@ -16,3 +16,12 @@ plane = pygame.transform.scale(pygame.image.load(os.path.join("assets", "plane1.
 
 BG1 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background1.png")), (WIDTH, HEIGHT))
 BG2 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background2.png")), (WIDTH, HEIGHT))
+
+mixer.init()
+bulletSound = pygame.mixer.Sound('assets/bullet.mp3')
+ed = pygame.mixer.Sound('assets/enemy1_down.mp3')
+gmover = pygame.mixer.Sound('assets/gameover.mp3')
+
+pygame.mixer.music.load('assets/game_music.mp3')
+pygame.mixer.music.play(-1, 0.0)
+pygame.mixer.music.set_volume(0.25)
