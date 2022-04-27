1728845239c74937dfe63465dfe750843d83f17f
JaysonFormentos <103914840+JaysonFormentos@users.noreply.github.com>
2022-04-27 20:41:29 +0800

    for bullet class

--- SkySweeper.py
+++ SkySweeper.py
@@ -26,6 +26,25 @@ pygame.mixer.music.load('assets/game_music.mp3')
 pygame.mixer.music.play(-1, 0.0)
 pygame.mixer.music.set_volume(0.25)
 
+class Bullts:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    def move(self, vel):
+        self.y += vel
+
+    def off_screen(self, height):
+        return not(self.y <= height and self.y >= 0)
+
+    def collision(self, obj):
+        return collide(self, obj)
+
 
 class Ship:
     COOLDOWN = 25
