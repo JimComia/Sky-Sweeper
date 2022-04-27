64529ccadd3e0b50fd416df7731470c7b41a7dd1
Honeycausapin <103179879+Honeycausapin@users.noreply.github.com>
2022-04-27 01:43:50 -0700

    for class ship/enemy

--- SkySweeper.py
+++ SkySweeper.py
@@ -25,3 +25,56 @@ gmover = pygame.mixer.Sound('assets/gameover.mp3')
 pygame.mixer.music.load('assets/game_music.mp3')
 pygame.mixer.music.play(-1, 0.0)
 pygame.mixer.music.set_volume(0.25)
+
+
+class Ship:
+    COOLDOWN = 25
+    
+
+    def __init__(self, x, y, health=100, score=0):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.score = score
+        self.ship_img = None
+        self.bullts_img = None
+        self.bulltss = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        window.blit(self.ship_img, (self.x, self.y))
+        for bullts in self.bulltss:
+            bullts.draw(window)
+
+    def move_bulltss(self, vel, obj):
+        self.cooldown()
+        for bullts in self.bulltss:
+            bullts.move(vel)
+            if bullts.off_screen(HEIGHT):
+                self.bulltss.remove(bullts)
+            elif bullts.collision(obj):
+                obj.health -= 10
+                self.bulltss.remove(bullts)
+
+    def cooldown(self):
+        if self.cool_down_counter >= self.COOLDOWN:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter > 0:
+            self.cool_down_counter += 1
+
+    def shoot(self):
+        
+        if self.cool_down_counter == 0:
+            bulletSound.play()
+            bullts = Bullts(self.x, self.y, self.bullts_img)
+            self.bulltss.append(bullts)
+            self.cool_down_counter = 1
+
+    def get_width(self):
+        return self.ship_img.get_width()
+
+    def get_height(self):
+        return self.ship_img.get_height()
+     
+    
+ 
