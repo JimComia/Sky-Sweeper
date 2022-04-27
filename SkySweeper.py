9f3f3ded1f5b4a67d0b56a247ac680b7e1703fb2
JaysonFormentos <103914840+JaysonFormentos@users.noreply.github.com>
2022-04-27 16:30:22 +0800

    for background

--- SkySweeper.py
+++ SkySweeper.py
@@ -13,3 +13,6 @@ pygame.display.set_caption("Sky Sweeper")
 astteroid1 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "aster1.png")), (150,150))
 
 plane = pygame.transform.scale(pygame.image.load(os.path.join("assets", "plane1.png")), (100,100))
+
+BG1 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background1.png")), (WIDTH, HEIGHT))
+BG2 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background2.png")), (WIDTH, HEIGHT))
