348fcf951c3c88ecba6a761a41c5a3ed191ca2d1
Jim <comia.133188@balayan.sti.edu.ph>
2022-04-28 08:17:30 +0800

    while run and main menu

--- SkySweeper.py
+++ SkySweeper.py
@@ -10,9 +10,12 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sky Sweeper")
 
 
+
 astteroid1 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "aster1.png")), (150,150))
 
 plane = pygame.transform.scale(pygame.image.load(os.path.join("assets", "plane1.png")), (100,100))
+
+bullets = pygame.transform.scale(pygame.image.load(os.path.join("assets", "bullet.png")), (30,30))
 
 BG1 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background1.png")), (WIDTH, HEIGHT))
 BG2 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background2.png")), (WIDTH, HEIGHT))
@@ -191,4 +194,74 @@ def main():
 
         pygame.display.update()
 
+    while run:
+        clock.tick(FPS)
+        redraw_window()
+
+        if player.health <= 0:
+            lost = True
+            lost_count += 1
+
+        if lost:
+            if lost_count > FPS * 3:
+                run = False
+            else:
+                continue
+
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, WIDTH - 100), random.randrange(-1500, -100))
+                enemies.append(enemy)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+        # for buttons
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] or keys[pygame.K_LEFT] and player.x - player_vel > 0:
+            player.x -= player_vel
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT] and player.x + player_vel + player.get_width() < WIDTH:
+            player.x += player_vel
+        if keys[pygame.K_w] or keys[pygame.K_UP] and player.y - player_vel > 0:
+            player.y -= player_vel
+        if keys[pygame.K_s] or keys[pygame.K_DOWN] and player.y + player_vel + player.get_height() + 15 < HEIGHT:
+            player.y += player_vel
+        if keys[pygame.K_SPACE]:
+            player.shoot()
+
+        for enemy in enemies[:]:
+            enemy.move(enemy_vel)
+
+            if collide(enemy, player):
+                player.health -= 10
+                ed.play()
+                enemies.remove(enemy)
+                player.score += 50
+            elif enemy.y + enemy.get_height() > HEIGHT:
+                player.health -= 10
+                ed.play()
+                enemies.remove(enemy)
+
+        player.move_bulltss(-bullts_vel, enemies)
+
+
+def main_menu():
+    title_font = pygame.font.SysFont("comicsans", 50)
+    run = True
+    while run:
+        WIN.blit(BG2, (0, 0))
+        title_label = title_font.render("Press the mouse to begin...", 1, (255, 255, 255))
+        WIN.blit(title_label, (WIDTH / 2 - title_label.get_width() / 2, 350))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                main()
+    pygame.quit()
+
+
+main_menu()
  
